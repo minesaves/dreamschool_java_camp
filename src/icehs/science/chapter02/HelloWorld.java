e3ce3f073d9e96feeabb7ced861f304ba89a48dd
학생44 <44@user-pc>
2019-05-05 14:11:24 +0900

    chapter02

---
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -0,0 +1,14 @@
+package icehs.science.chapter02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("world");
+		System.out.println("hahaha");
+		System.out.println("nice to meet you");
+		// TODO Auto-generated method stub
+
+	}
+
+}
