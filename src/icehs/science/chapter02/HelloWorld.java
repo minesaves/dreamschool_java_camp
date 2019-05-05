654cdf05c4f41026856c750d827194fb75994999
학생44 <44@user-pc>
2019-05-05 14:15:38 +0900

    java

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -3,12 +3,11 @@ package icehs.science.chapter02;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("world");
-		System.out.println("hahaha");
-		System.out.println("nice to meet you");
+		System.out.println("hello"); //나의
+		System.out.println("world"); //첫
+		System.out.println("hahaha");//작품
+		System.out.println("nice to meet you"); //이다
 		// TODO Auto-generated method stub
-
 	}
 
 }
